cbf402ef9c00564a05e4492cd0f75c11c0ab5c45
William <williamcostacardoso94@gmail.com>
2025-04-10 22:12:34 -0300

    chore: configurando html, css, webpack plugins

---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict"
+
+// CSS
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     target: "web",
@@ -9,4 +11,37 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        port: 3000,
+        open: true,
+        liveReload: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+            favicon: path.resolve("src", "assets", "scissors.svg"),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: path.resolve(__dirname, "dist", "src", "assets")
+                }
+            ]
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    }
 }
