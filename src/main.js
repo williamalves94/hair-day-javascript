ee8e924ce8ed1ac317ec9288e1f3ff3a25725408
William <williamcostacardoso94@gmail.com>
2025-04-14 00:19:45 -0300

    feat: formatando a data

--- src/main.js
+++ src/main.js
@@ -7,3 +7,5 @@ import "./libs/dayjs.js"
 import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
+
+import "./modules/form/submit.js"
---
+++ src/modules/form/submit.js
@@ -0,0 +1,20 @@
+import dayjs from "dayjs"
+
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+
+// date atual para o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+// carrega a data atual
+selectedDate.value = inputToday
+
+// define a data mínima como sendo a data atual
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    console.log("ok")
+}
