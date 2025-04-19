e0c6329540ff134e734a9a6052fde0506e97a837
William <williamcostacardoso94@gmail.com>
2025-04-19 16:21:24 -0300

    feat: renderizando os horarios disponiveis

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import { openingHours } from "../../utils/opening-hours"
 import dayjs from "dayjs"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
     const opening = openingHours.map((hour) => {
         // recupera somente a hora.
@@ -16,5 +18,34 @@ export function hoursLoad({ date }) {
 
     })
 
+    // renderiza os horarios
+    opening.forEach(({ hour, available }) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+
+        if (hour === "09:00") {
+            hourHeaderAdd("Manhã")
+        } else if (hour === "13:00") {
+            hourHeaderAdd("Tarde")
+        } else if (hour === "18:00") {
+            hourHeaderAdd("Noite")
+        }
+
+        hours.append(li)
+    })
+
 }
+
+function hourHeaderAdd(title) {
+    const header = document.createElement("li")
+    header.classList.add("hour-period")
+    header.textContent = title
+
+    hours.append(header)
+}
+
 
